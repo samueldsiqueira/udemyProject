55bfa56d61a8f08b01ee8f2d46d38bd513040a99
samueldsiqueira <41975168+samueldsiqueira@users.noreply.github.com>
2022-01-24 12:11:25 -0300

    save-question-on-db

--- database/Pergunta.js
+++ database/Pergunta.js
@@ -1,12 +1,15 @@
 const Sequelize = require("sequelize");
 const connection = require("./database");
 
-// cria a variavel que recebe o model > chama a conexão > poem o nome da tabela > abre o JSON  define os campos
-const Pergunta = connection.define("pergunta", {
+// cria a variavel que recebe o model > chama a conexão > poem o nome da tabela > abre o JSON > define os campos
+const Pergunta = connection.define("perguntas", {
   titulo: { type: Sequelize.STRING, allowNull: false },
   descricao: { type: Sequelize.TEXT, allowNull: false },
 });
 
+// o comando force: false QUER DIZER PARA O CÓDIGO que se a tabela "pergunta" já existir ele não vai forçar ela a ser criada, o then é executado quando a tabela é criada.
 Pergunta.sync({ force: false }).then(() => {
   console.log("tabela criada!");
 });
+
+module.exports = Pergunta;
