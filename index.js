3ab75874e35e5b17839ae8a92d2c5e973a87f01f
samueldsiqueira <41975168+samueldsiqueira@users.noreply.github.com>
2022-01-24 12:09:54 -0300

    save-question-on-db

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
-const perguntaModel = require("./database/Pergunta");
+const Pergunta = require("./database/Pergunta");
 // Database
 connection
   .authenticate()
@@ -31,12 +31,16 @@ app.get("/perguntar", (req, res) => {
   res.render("perguntar");
 });
 
+//  Pergunta.create(); = "INSERT INTO perguntas ... Pergunta"
 app.post("/salvarpergunta", (req, res) => {
   var titulo = req.body.titulo;
   var descricao = req.body.descricao;
-  res.send(
-    "Formulário recebido! titulo " + titulo + " " + "descricao" + descricao,
-  );
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao,
+  }).then(() => {
+    res.redirect("/");
+  });
 });
 
 app.listen(8080, () => {
